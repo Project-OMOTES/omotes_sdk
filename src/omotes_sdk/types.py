7bd1967abd74cb05c67391c1cafa993d66688765
Cheng-Kai Wang <cheng-kai.wang@tno.nl>
2024-06-03 15:41:57 +0200

    Allow parse_workflow_config_parameter() to expect int type and round float value to int if specified

--- src/omotes_sdk/types.py
+++ src/omotes_sdk/types.py
@@ -1,4 +1,4 @@
 from typing import List, Union, Mapping
 
-ParamsDictValues = Union[List["ParamsDictValues"], "ParamsDict", None, float, str, bool]
+ParamsDictValues = Union[List["ParamsDictValues"], "ParamsDict", None, float, int, str, bool]
 ParamsDict = Mapping[str, ParamsDictValues]
