ace31b384c57cd09d60f5a03195997314ddc21c1
Sebastiaan la Fleur <sebastiaan@lfse.nl>
2024-12-11 10:59:32 +0100

    90: Update to latest python template.

--- ci/linux/install_dependencies.sh
+++ ci/linux/install_dependencies.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [[ "$OSTYPE" != "win32" && "$OSTYPE" != "msys" ]]; then
   echo "Activating .venv first."
   . .venv/bin/activate
 fi
 
-pip-compile -U --extra=dev --output-file=requirements.txt pyproject.toml
-pip install -r ./requirements.txt
+pip-sync ./dev-requirements.txt ./requirements.txt
